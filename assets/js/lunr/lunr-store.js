3a57a67dee2e8e9daea13bbc9e6dedd01dcca61a
bitjockey42 <bitjockey42@users.noreply.github.com>
2023-04-12 14:04:05 +0000

    jekyll build from Action d1e17ae49c7b52cec98b2b8e2d32d45bb977be12

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "New Blog",
+        "excerpt":"This will be our new blog for sharing dev notes and updates on our projects at SolipsisAI.  ","categories": ["update"],
+        "tags": [],
+        "url": "/2022/06/14/new-blog/",
+        "teaser": null
+      },{
+        "title": "Our Objectives",
+        "excerpt":"With HuggingFace’s Transformers and Tokenizers libraries, many smaller businesses and individual researchers have become empowered to build their own Natural Language Processing (NLP) applications. It helps that (for the most part) the community itself around NLP research and application encourages an open-source exchange of ideas, tools, studies, even data. Many...","categories": ["project"],
+        "tags": [],
+        "url": "/2022/06/14/our-objectives/",
+        "teaser": null
+      },{
+        "title": "Diarist now available for download",
+        "excerpt":"The alpha version of our Diarist app is now available for macOS (intel) and Linux (as an AppImage). Download it here. This alpha includes these features: Autosaving entries Analyzing the emotion and sentiment of the entry Completely offline classification with TensorFlow Lite Coming soon: The iOS release Dashboard for summarizing...","categories": ["update"],
+        "tags": [],
+        "url": "/2023/04/12/diarist-alpha-available/",
+        "teaser": null
+      }]
